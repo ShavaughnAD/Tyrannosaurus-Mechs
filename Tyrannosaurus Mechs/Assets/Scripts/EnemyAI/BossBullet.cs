a9b9c6a296d3c0da1300ed284c12d6b7936b041d
Chase <CPGriffiths@student.fullsail.edu>
2020-06-07 13:37:02 -0400

    Boss Placeholder and Scripts in Test Scene

---
+++ Tyrannosaurus Mechs/Assets/Scripts/EnemyAI/BossBullet.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    private float moveSpeed;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    void Start()
+    {
+        moveSpeed = 3f;
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime); 
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
