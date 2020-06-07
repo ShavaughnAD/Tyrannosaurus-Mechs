322334549479e190e33248aed3b0acaa2c7fd7b1
Chase <CPGriffiths@student.fullsail.edu>
2020-06-07 16:49:37 -0400

    Created first playable ship that does can fire projectiles
    Deleted Meta files.

---
+++ Tyrannosaurus Mechs/Assets/Scripts/PlayerShip01Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip01Shoot : MonoBehaviour
+{
+    public GameObject PlayerShip01Bullet;
+    public GameObject bulletPosition01;
+    public GameObject bulletPosition02;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet01 = (GameObject)Instantiate(PlayerShip01Bullet);
+            bullet01.transform.position = bulletPosition01.transform.position;
+
+            GameObject bullet02 = (GameObject)Instantiate(PlayerShip01Bullet);
+            bullet02.transform.position = bulletPosition02.transform.position;
+        }
+    }
+}
