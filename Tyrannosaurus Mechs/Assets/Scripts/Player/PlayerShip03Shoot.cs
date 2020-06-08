50328bd3a86b0e25297816dd40531be162b9966e
Chase <CPGriffiths@student.fullsail.edu>
2020-06-08 09:44:23 -0400

    Added a 3rd player ship that shoots a spread of bullets

---
+++ Tyrannosaurus Mechs/Assets/Scripts/Player/PlayerShip03Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip03Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] firePoints;
+    [SerializeField]
+    private GameObject projectilePrefab;
+    [SerializeField]
+    private float launchForce = 700f;
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LaunchProjectile();
+        }
+    }
+
+    private void LaunchProjectile()
+    {
+        foreach (var firePoint in firePoints)
+        {
+            var projectileInstance = Instantiate(
+                projectilePrefab,
+                firePoint.position,
+                firePoint.rotation);
+
+            //projectileInstance.AddForce(firePoint.forward * launchForce);
+        }
+    }
+}
