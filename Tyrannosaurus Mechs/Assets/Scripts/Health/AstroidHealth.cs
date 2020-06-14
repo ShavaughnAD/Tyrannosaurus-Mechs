0a9cb8ce17275d0d8833bc3af4a75fa45a9ed9a8
RonnieDS <58788535+RonnieDS@users.noreply.github.com>
2020-06-14 16:57:59 -0400

    Medium Astroid, Astroid health script
    -Made a Variation of the Health script for Astroids to explode when shot and split.
    -Made the medium sized Astroids turn into small ones as well.
    -Made a new Astroidmedium sized.Now Big astroids split into a the new medium sized Astroids.

---
+++ Tyrannosaurus Mechs/Assets/Scripts/Health/AstroidHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidHealth : Health
+{
+    public GameObject astroidMedium;
+    public Transform spawnP;
+
+
+    public override void TakeDamage(float damageAmount)
+    {
+        base.TakeDamage(damageAmount);
+        currentHealth = Mathf.Clamp(currentHealth - damageAmount, 0, maxHealth);
+        if (currentHealth == 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Instantiate(astroidMedium, spawnP.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
