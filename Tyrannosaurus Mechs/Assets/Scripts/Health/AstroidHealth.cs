d48c63cb6904c27e74f7b8dcad98f99a2a6a1ff9
ShavaughnAD <shavaughnd1@gmail.com>
2020-06-15 21:46:27 -0400

    Replaced old bullets with new ones. Changed Ship Sprites

--- Tyrannosaurus Mechs/Assets/Scripts/Health/AstroidHealth.cs
+++ Tyrannosaurus Mechs/Assets/Scripts/Health/AstroidHealth.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AstroidHealth : Health
 {
     public GameObject astroidMedium;
     public Transform spawnP;
 
+    #region BlinkingEffect Variables
+
+    [Range(0.1f, 0.5f)]
+    public float blinkEffectTime = 0.5f; //The lower this value, the faster the blink
+    Material matBlink;
+    Material matDefault;
+    SpriteRenderer spriteRend;
+
+    #endregion
+
+    void Start()
+    {
+        spriteRend = GetComponent<SpriteRenderer>();
+        matBlink = Resources.Load("Effects/Blink", typeof(Material)) as Material;
+        matDefault = spriteRend.material;
+    }
 
     public override void TakeDamage(float damageAmount)
     {
         base.TakeDamage(damageAmount);
         currentHealth = Mathf.Clamp(currentHealth - damageAmount, 0, maxHealth);
+        spriteRend.material = matBlink;
         if (currentHealth == 0)
         {
             Death();
+        }
+        else
+        {
+            Invoke("ResetMaterial", blinkEffectTime);
         }
     }
 
@@ -26,4 +45,10 @@ public class AstroidHealth : Health
             Destroy(gameObject);
         }
     }
+
+    void ResetMaterial()
+    {
+        spriteRend.material = matDefault;
+    }
+
 }
