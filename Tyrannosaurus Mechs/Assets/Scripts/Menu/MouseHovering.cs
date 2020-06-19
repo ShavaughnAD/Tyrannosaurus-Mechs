a4818068306133bda766b853747bb7234353fb8d
ToxicUnitatoUs <64419115+ToxicUnitatoUs@users.noreply.github.com>
2020-06-19 10:49:36 -0400

    Implemented art into menus
    Implemented the art assets into the options menu

--- Tyrannosaurus Mechs/Assets/Scripts/Menu/MouseHovering.cs
+++ Tyrannosaurus Mechs/Assets/Scripts/Menu/MouseHovering.cs
@@ -11,23 +11,33 @@ public class MouseHovering : MonoBehaviour
 
     public AudioSource AudioSrc;
 
-    //public GameObject _Open;
+    public GameObject _Open;
+    public GameObject _closed;
+
+    void Start()
+    {
+        _Open.SetActive(false);
+        _closed.SetActive(true);
+    }
 
     public void Hovering()
     {
         AudioSrc.PlayOneShot(myButtonClick);
 
-        //_Open.SetActive(true);
+        _Open.SetActive(true);
+        _closed.SetActive(false);
     }
 
     public void OffHovering()
     {
-        //_Open.SetActive(false);
+        _Open.SetActive(false);
+        _closed.SetActive(true);
     }
 
     public void HoverPressed()
     {
-        //_Open.SetActive(false);
+        _Open.SetActive(false);
+        _closed.SetActive(true);
     }
 
     public void ButtonDown()
