4fc645233953feb52099ccf513feca7d5f6e244e
Chase <CPGriffiths@student.fullsail.edu>
2020-06-26 12:55:13 -0400

    Added 3rd enemy type as well as the enemy spawner for that type

---
+++ Tyrannosaurus Mechs/Assets/Scripts/EnemyAI/Enemy03Gun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy03Gun : MonoBehaviour
+{
+    public GameObject EnemyBullet;
+
+    private GameObject target;
+    private Vector3 targetPoint;
+    private Quaternion targetRotation;
+
+    void Start()
+    {
+        Invoke("FireEnemyBullet", 1f);
+        target = GameManager.gameManager.playerShipMovement.gameObject;
+    }
+
+    
+    void Update()
+    {
+        //targetPoint = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position;
+        //targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
+    }
+
+    void FireEnemyBullet()
+    {
+        GameObject playerShip = GameObject.FindGameObjectWithTag("Player");
+
+        if(playerShip != null)
+        {
+            GameObject bullet = (GameObject)Instantiate(EnemyBullet);
+
+            bullet.transform.position = transform.position;
+
+            Vector2 direction = playerShip.transform.position - bullet.transform.position;
+
+            bullet.GetComponent<EnemyBullet>().SetDirection(direction);
+        }
+    }
+}
