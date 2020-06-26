457d6a004180855774581120eec6abe652ee7115
ShavaughnAD <shavaughnd1@gmail.com>
2020-06-26 18:43:28 -0400

    Added Ship 4. In Character Select Also

---
+++ Tyrannosaurus Mechs/Assets/Scripts/PlayerShip4Beam.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShip4Beam : PlayerMovement
+{
+    public Transform beamSpawnPoint;
+    public GameObject laserBeam;
+    public float maxCanisterSize = 100;
+    public float currentCanisterSize = 100;
+    public float canisterRefillRate = 5;
+    public float canisterDecayRate = .010f;
+
+    bool isFiring = false;
+
+    void Awake()
+    {
+        currentCanisterSize = maxCanisterSize;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (isFiring == false)
+        {
+            currentCanisterSize += canisterRefillRate * Time.deltaTime;
+            currentCanisterSize = Mathf.Clamp(currentCanisterSize, 0, maxCanisterSize);
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0) && currentCanisterSize >= 1)
+        {
+            laserBeam.SetActive(true);
+            currentCanisterSize -= canisterDecayRate;
+            isFiring = true;
+        }
+        else
+        {
+            laserBeam.SetActive(false);
+            isFiring = false;
+        }
+    }
+}
